212e3f3992d3c6f4a9a57dbc6625c61be4ca89c3
javaexpresschannel <javaexpresschannel@gmail.com>
2024-02-03 09:05:15 +0530

    functionalevel

---
+++ 02-react-functionallevelcomponent/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+// jsx
+function App() {
+  return (
+    <div >
+      <h1>Welcome to React JS Course</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-react-functionallevelcomponent/src/First.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function First() {
+
+    return(
+        <div>
+            <h1>Welcome to Functional Level Component </h1>
+             
+        </div>
+    );
+}
+
+
+export default First;
